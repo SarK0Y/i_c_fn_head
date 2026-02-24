7908f23eab9a7964f3b66d058d5285da176aabb3
SarK0Y <sark0y@protonmail.com>
2026-02-25 00:21:59 +0300

    ...

--- src/extension.ts
+++ src/extension.ts
@@ -198,13 +198,12 @@ export function c_fn_body(doc: string, uri: vscode.Uri, symbols: &vscode.SymbolI
 	const one_line_comment: RegExp = /^[/]{2}/;
 	const open_comment: RegExp = /^\/\*/;
 	const close_comment: RegExp = /\*\/$/;
-	const open_block: RegExp = /^\{/;
-	const close_block: RegExp = /\}$/;
+	const open_block: RegExp = /(^\{([/]{2})?(\/\*)?)|(\{([/]{2})?(\/\*)?$)/;
+	const close_block: RegExp = /(^\}([/]{2})?(\/\*)?)|(\}([/]{2})?(\/\*)?$)/;
 	const tst_class: RegExp = /^class\s/; 
 	let within_comment: boolean = false;
 	let start_class: number | null = null;
 	let close_class: number | null = null;
-	let lnum: number = 0;
 	let block_state: number = 0;
 	let fn_head: string = "";
 	for (let i = 0; i < lines.length; i++) {
@@ -235,7 +234,8 @@ export function c_fn_body(doc: string, uri: vscode.Uri, symbols: &vscode.SymbolI
 			);
 			continue;
 		}
-
+		block_state -= open_block.test(lines[i]) ? 1 : 0;
+		block_state += close_block.test(lines[i]) ? 1 : 0;
 	}
 }
 export function add_symb(
