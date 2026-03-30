118f5a5c5313e920bad3d8d1a6bf2f6bbb18cd9c
SarK0Y <sark0y@protonmail.com>
2026-03-31 01:30:38 +0300

    init gears :)

--- src/init.ts
+++ src/init.ts
@@ -3,13 +3,26 @@ import { i_c_fn_head_opts } from './faav';
 export async function init() {
     if (!i_c_fn_head_opts.been_set) { return; }
     const uri = await vscode.workspace.findFiles(
-        '**/*.{langsName.file_exts}',
+        '**/i_c_fn_head.opts',
         "", /* exclude none path */
         1 /* only one result */
     );
     try {
         const doc = await vscode.workspace.openTextDocument(uri[0]);
-        const txt = doc.getText();
+        txt._0 = doc.getText();
+        set_lang("D");
+        set_lang("Rust");
+        set_lang("C");
+        set_lang("CPP");
     }
     catch { }
+}
+function run_opt(key: string): RegExp {
+    return /\/\/\s*run\s+{key}\s*\/\//;
+}
+function set_lang(name: string) {
+    if (run_opt(name).test(txt._0)) { eval("i_c_fn_head_opts.provide_lang_" + name + "= true"); }
+}
+class txt {
+    static _0: string = "";
 }
